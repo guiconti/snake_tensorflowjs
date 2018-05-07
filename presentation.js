01a56573eb8f7fb667fdea289e33655bf6e2a6b3
guiconti <guibasconti@gmail.com>
2018-05-07 00:49:34 +0000

    Fix v10 reference errors

--- presentation.js
+++ presentation.js
@@ -29,7 +29,7 @@ Multi AI learning while using the same brain
 Neuro evolution and [Flappy bird](v9/index2.html) variation
 
 ## [V10](v10/snake)
-Neuro evolution at Snake[v10/snake] and [Flappy bird](v10/flappy) variation
+Neuro evolution at [Snake](v10/snake) and [Flappy bird](v10/flappy) variation
 `;
 const htmlBody = document.getElementById("markdown");
 htmlBody.innerHTML = converter.makeHtml(text);
